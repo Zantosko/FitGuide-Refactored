d5e9c4aad221966048e3e0d57e55bbee5f74a82e
Zach Antosko <zachary.antosko@gmail.com>
2021-08-29 16:07:01 -0500

    started on calorie calculator

--- client/src/Components/CalorieNeeds.tsx
+++ client/src/Components/CalorieNeeds.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { Container } from './Styled-Components/CalorieNeedsStyles';
+import {
+	Container,
+	Title,
+	Form,
+} from './Styled-Components/CalorieNeedsStyles';
 
 interface CalorieNeedsProps {}
 
 export const CalorieNeeds: React.FC<CalorieNeedsProps> = () => {
 	return (
 		<Container>
-			<h1>Calorie Calculator</h1>
+			<Title>Calorie Calculator</Title>
+			<Form action=''>
+				<div>
+					<label htmlFor='gender'>
+						<input type='radio' name='gender' />
+						<span>Male</span>
+					</label>
+					<label htmlFor='gender'>
+						<input type='radio' name='gender' />
+						<span>Female</span>
+					</label>
+				</div>
+				<input type='text' />
+				<input type='text' />
+				<select name='' id=''>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+					<option value=''></option>
+				</select>
+			</Form>
 		</Container>
 	);
 };
--- client/src/Components/Styled-Components/CalorieNeedsStyles.tsx
+++ client/src/Components/Styled-Components/CalorieNeedsStyles.tsx
@@ -3,6 +3,25 @@ import styled from 'styled-components';
 export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
 	align-items: center;
+	height: 100vh;
+	padding: 2rem;
+`;
+
+export const Title = styled.h1`
+	color: #29bbad;
+	font-size: 3rem;
+	margin-bottom: 2rem;
+`;
+
+export const Form = styled.form`
+	margin: 1rem;
+	padding: 1rem;
+	background-color: #f1f1f1;
+	height: 60%;
+	width: 50%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	box-shadow: 0.1em 0.1em 1em rgba(0, 0, 0, 0.4);
 `;
