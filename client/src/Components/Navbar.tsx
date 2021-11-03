bc3c4084ecc4907637a61835633eb5a2879cf0f2
Zach Antosko <zachary.antosko@gmail.com>
2021-11-02 19:42:46 -0500

    updated nav

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -25,7 +25,6 @@ interface NavbarProps {}
 export const Navbar = (props: NavbarProps) => {
 	const [showMenu, setShowMenu] = useState<boolean>(false);
 
-	//* Custom hook to access window size changes
 	const size = useWindowSize();
 
 	let menu;
